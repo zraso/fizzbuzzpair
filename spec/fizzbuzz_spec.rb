f9e2b53beac7710793598ff27a8f3ec5d8f598dd
Kat <vampirebatkat@gmail.com>
2019-09-09 17:12:20 +0100

    second test passing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,11 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
-        expect (fizzbuzz(3)).to eq "fizz"
-    end
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq('fizz')
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq('buzz')
+  end
 end
